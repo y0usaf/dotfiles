d82a93ddb6d5d65bc4d6ecb8d87e53a177558cee
thami <OA99@Outlook.com>
2024-11-15 02:05:59 -0500

    Sync dotfiles: Fri 15 Nov 2024 02:05:59 AM EST

--- .config/fabric/notifications.py
+++ .config/fabric/notifications.py
@@ -1,17 +1,19 @@
 from typing import cast
+
 from fabric import Application
-from fabric.widgets.wayland import WaylandWindow
 from fabric.widgets.box import Box
 from fabric.widgets.label import Label
 from fabric.widgets.image import Image
 from fabric.widgets.button import Button
-from fabric.widgets.eventbox import EventBox
+from fabric.widgets.wayland import WaylandWindow
 from fabric.notifications import Notifications, Notification
-from fabric.utils import get_relative_path, invoke_repeater
-from gi.repository import GdkPixbuf, Gdk
+from fabric.utils import invoke_repeater, get_relative_path
+
+from gi.repository import GdkPixbuf, GLib
+
 
 NOTIFICATION_WIDTH = 360
-NOTIFICATION_IMAGE_SIZE = 48
+NOTIFICATION_IMAGE_SIZE = 64
 NOTIFICATION_TIMEOUT = 5 * 1000
 
 
@@ -20,19 +22,21 @@ class NotificationWidget(Box):
         super().__init__(
             size=(NOTIFICATION_WIDTH, -1),
             name="notification",
-            spacing=8,
+            spacing=12,
             orientation="v",
+            margin=8,
             **kwargs,
         )
 
         self._notification = notification
 
-        # Create an EventBox to handle clicks
-        event_box = EventBox(
-            events=Gdk.EventMask.BUTTON_PRESS_MASK,
-        )
+        GLib.timeout_add(NOTIFICATION_TIMEOUT, self._on_timeout)
 
-        body_container = Box(spacing=4, orientation="h")
+        body_container = Box(
+            spacing=8,
+            orientation="h",
+            margin=4,
+        )
 
         if image_pixbuf := self._notification.image_pixbuf:
             body_container.add(
@@ -41,65 +45,84 @@ class NotificationWidget(Box):
                         NOTIFICATION_IMAGE_SIZE,
                         NOTIFICATION_IMAGE_SIZE,
                         GdkPixbuf.InterpType.BILINEAR,
-                    )
+                    ),
+                    margin_end=8,
                 )
             )
 
         text_container = Box(
-            spacing=4,
+            spacing=6,
             orientation="v",
-            children=[
-                Box(
-                    orientation="h",
-                    children=[
-                        Label(
-                            label=self._notification.summary,
-                            ellipsization="middle",
-                        )
-                        .build()
-                        .add_style_class("summary")
-                        .unwrap(),
-                    ],
-                    h_expand=True,
-                    v_expand=True,
-                ).build(
-                    lambda box, _: box.pack_end(
-                        Button(
-                            image=Image(
-                                icon_name="close-symbolic",
-                                icon_size=18,
-                            ),
-                            v_align="center",
-                            h_align="end",
-                            on_clicked=lambda *_: self._notification.close(),
-                        ),
-                        False,
-                        False,
-                        0,
-                    )
-                ),
-                Label(
-                    label=self._notification.body,
-                    line_wrap="word-char",
-                    v_align="start",
-                    h_align="start",
-                )
-                .build()
-                .add_style_class("body")
-                .unwrap(),
-            ],
             h_expand=True,
             v_expand=True,
         )
 
+        header = Box(
+            orientation="h",
+            h_expand=True,
+            margin_bottom=4,
+        )
+
+        summary_label = (
+            Label(
+                label=self._notification.summary,
+                ellipsization="middle",
+                h_align="start",
+                h_expand=True,
+            )
+            .build()
+            .add_style_class("summary")
+            .unwrap()
+        )
+
+        close_button = Button(
+            image=Image(
+                icon_name="close-symbolic",
+                icon_size=18,
+            ),
+            v_align="center",
+            h_align="end",
+            margin_start=8,
+            on_clicked=lambda *_: self._notification.close(),
+        )
+
+        header.add(summary_label)
+        header.add(close_button)
+        text_container.add(header)
+
+        text_container.add(
+            Label(
+                label=self._notification.body,
+                line_wrap="word-char",
+                v_align="start",
+                h_align="start",
+            )
+            .build()
+            .add_style_class("body")
+            .unwrap()
+        )
+
         body_container.add(text_container)
-        event_box.add(body_container)
-        self.add(event_box)
+        self.add(body_container)
 
-        # Connect click handler
-        event_box.connect("button-press-event", self._on_click)
+        if actions := self._notification.actions:
+            self.add(
+                Box(
+                    spacing=4,
+                    orientation="h",
+                    children=[
+                        Button(
+                            h_expand=True,
+                            v_expand=True,
+                            label=action.label,
+                            on_clicked=lambda *_, action=action: action.invoke(),
+                        )
+                        for action in actions
+                    ],
+                )
+            )
 
-        notification.connect(
+        self._notification.connect(
             "closed",
             lambda *_: (
                 parent.remove(self) if (parent := self.get_parent()) else None,
@@ -107,32 +130,10 @@ class NotificationWidget(Box):
             ),
         )
 
-        invoke_repeater(
-            NOTIFICATION_TIMEOUT,
-            lambda: notification.close("expired"),
-            initial_call=False,
-        )
-
-    def _on_click(self, widget, event):
-        """Handle click events on the notification"""
-        if event.button == 1:  # Left click
-            # If there are actions, invoke the default action
-            if self._notification.actions:
-                # The default action is typically the first one with key "default"
-                for action in self._notification.actions:
-                    if action.key == "default":
-                        action.invoke()
-                        break
-                else:  # If no default action found, use the first action
-                    self._notification.actions[0].invoke()
-
-            # Emit action-invoked signal
-            self._notification.action_invoked("default")
-
-            # Close the notification after handling the action
-            self._notification.close("dismissed")
-            return True
-        return False
+    def _on_timeout(self):
+        """Handle the notification timeout by closing it."""
+        self._notification.close()
+        return False  # Don't repeat the timeout
 
 
 if __name__ == "__main__":
@@ -140,7 +141,7 @@ if __name__ == "__main__":
         "notifications",
         WaylandWindow(
             margin="8px 8px 8px 8px",
-            anchor="top right",
+            anchor="top",  # Changed to "top" for center positioning
             child=Box(
                 size=2,
                 spacing=4,
@@ -162,5 +163,5 @@ if __name__ == "__main__":
         ),
     )
 
-    app.set_stylesheet_from_file(get_relative_path("./style.css"))
+    app.hold()  # Keep the application running
     app.run()
